17b6761962e92f36aa5dcd6a494f27a102f798df
Michael <michaelkatom10@gmail.com>
2020-01-05 00:54:34 +0200

    added sign in cookies, still second login doesnt work properly

--- my_selenium.py
+++ my_selenium.py
@@ -1,7 +1,4 @@
-import pickle
 import json
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 import constants
 
@@ -9,6 +6,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 import os.path
+
 
 def save_obj(obj, name):
     with open(name + '.txt', 'w') as f:
@@ -19,27 +17,26 @@ def load_obj(name):
     with open(name + '.txt', 'r') as f:
         return json.load(f)
 
+
 def start_login(email, password):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(constants.WEB_APP_URL)
     fileName = 'cookies'
 
-    if os.path.isfile(fileName+".txt"):
+    if os.path.isfile(fileName + ".txt"):
         driver.delete_all_cookies()
         cookies = load_obj(fileName)
-        print(cookies)
         for cookie in cookies:
             cookie['expiry'] = int(cookie['expiry'])
-
             driver.add_cookie(cookie)
         driver.get(constants.WEB_APP_URL)
         driver.implicitly_wait(20)
-        el = driver.find_elements_by_css_selector(".ut-login .ut-login-content .btn-standard")[0]
-        print(el)
-        driver.implicitly_wait(60)
-        el.click()
+        # el = driver.find_elements_by_css_selector(".ut-login .ut-login-content .btn-standard")[0]
+        # print(el)
+        # driver.implicitly_wait(60)
+        # el.click()
 
-        #Log In btn
+    # cookies file was not found - log in the first time
 
     else:
         driver.get("https://signin.ea.com/p/web2/login?execution=e1639509631s1&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Fprompt%3Dlogin%26accessToken%3Dnull%26client_id%3DFIFA-20-WEBCLIENT%26response_type%3Dtoken%26display%3Dweb2%252Flogin%26locale%3Den_US%26redirect_uri%3Dhttps%253A%252F%252Fwww.easports.com%252Ffifa%252Fultimate-team%252Fweb-app%252Fauth.html%26release_type%3Dprod%26scope%3Dbasic.identity%2Boffline%2Bsignin%2Bbasic.entitlement%2Bbasic.persona")
@@ -50,28 +47,24 @@ def start_login(email, password):
         passwordField.send_keys(password)
         logInButton.click()
         driver.find_element_by_xpath("//input[@name='codeType' and @value='SMS']").click()
-        #send the email verfication
+        # send the email verfication
         driver.find_element_by_class_name("btn-next").click()
 
         input1 = input("Enter your code ")
         if input1:
             driver.find_element_by_id("oneTimeCode").send_keys(input1)
             driver.find_element_by_id("btnSubmit").click()
-            save_obj(driver.get_cookies(),fileName)
+            input1 = input("ENter:")
+            # save_obj(driver.get_cookies(), fileName)
+            eaCookies = driver.get_cookies()
+            driver.get("https://signin.ea.com/p/web2/login?execution=e1639509631s1&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Fprompt%3Dlogin%26accessToken%3Dnull%26client_id%3DFIFA-20-WEBCLIENT%26response_type%3Dtoken%26display%3Dweb2%252Flogin%26locale%3Den_US%26redirect_uri%3Dhttps%253A%252F%252Fwww.easports.com%252Ffifa%252Fultimate-team%252Fweb-app%252Fauth.html%26release_type%3Dprod%26scope%3Dbasic.identity%2Boffline%2Bsignin%2Bbasic.entitlement%2Bbasic.persona")
+            signInCookies = driver.get_cookies()
 
-
-    # """get the confirmation password"""
-    # driver.implicitly_wait(20)
-
-
-    # confirmationCode = checkMailForConfirmation(driver, email)
-
-    #     checkMainForConfirmation(email)
-    # driver.close()
-
-
-def checkMailForConfirmation(driver, email):
-    #new tab
-    # driver.execute_script('''window.open("https://accounts.google.com/ServiceLogin?service=mail#identifier","_blank");''')
-    confirmationCode = ""
-    return confirmationCode
+            print("loginCookies",signInCookies)
+            print("eaCookies",eaCookies)
+            for cookie in signInCookies:
+                cookie['expiry'] = int(cookie['expiry'])
+                if cookie not in eaCookies:
+                    eaCookies.append(cookie)
+            save_obj(eaCookies,fileName)
+            driver.back()
