466a96fd1c9ad7a6c58217db11754233da1d4f33
Michael <michaelkatom10@gmail.com>
2020-03-07 12:46:21 +0200

    fixed increased decrease 200 coins bug

--- my_selenium.py
+++ my_selenium.py
@@ -30,16 +30,21 @@ def check_auth_status(func):
 def set_auth_status(self, is_auth):
     self.is_authenticated = is_auth
 
+
 def set_status_code(self, code):
     self.statusCode = code
     return server_status_messages.STATUS_CODE_SET_CORRECTLY, 200
 
+
 def initialize_driver(self):
     self.driver = webdriver.Chrome(ChromeDriverManager().install())
     self.driver.get(constants.WEB_APP_URL)
+    # self.driver.find_element_by_xpath().
+
 
 def initialize_element_actions(self):
     self.element_actions = ElementActions(self.driver)
+
 
 def login_with_cookies(self, password):
     self.driver.delete_all_cookies()
@@ -56,6 +61,7 @@ def login_with_cookies(self, password):
     self.element_actions.execute_element_action(elements.BTN_NEXT, ElementCallback.CLICK)
     set_auth_status(self, True)
 
+
 def login_first_time(self, email, password):
     self.driver.get(constants.SIGN_IN_URL)
     self.element_actions.execute_element_action(elements.EMAIL_FIELD, ElementCallback.SEND_KEYS, email)
@@ -66,6 +72,7 @@ def login_first_time(self, email, password):
     # send the sms verfication
     self.element_actions.execute_element_action(elements.BTN_NEXT, ElementCallback.CLICK)
 
+
 def wait_for_code(self):
     while self.statusCode is '':
         pass
@@ -73,6 +80,7 @@ def wait_for_code(self):
     # status code is set
     self.element_actions.execute_element_action(elements.ONE_TIME_CODE_FIELD, ElementCallback.SEND_KEYS, self.statusCode)
     self.element_actions.execute_element_action(elements.BTN_NEXT, ElementCallback.CLICK)
+
 
 def remember_logged_in_user(self):
     eaCookies = self.driver.get_cookies()
@@ -88,13 +96,31 @@ def remember_logged_in_user(self):
     saveToCookiesFile(eaCookies, constants.COOKIES_FILE_NAME)
     self.driver.back()
 
+
 def remove_unexpected_popups(self):
     popup = self.element_actions.get_clickable_element(elements.VIEW_MODAL_CONTAINER)
     if popup:
         self.driver.execute_script(selenium_scripts.REMOVE_ELEMENT, popup)
 
+
+def decrease_increase_min_price(self, increase_price):
+    # check if price can be decreased
+    el = self.element_actions.get_clickable_element(elements.DECREASE_PRICE_BTN)
+    can_be_decreased = el.is_enabled()
+    max_bin_price = self.element_actions.get_clickable_element(elements.MAX_BIN_PRICE_INPUT)
+    max_bin = max_bin_price.get_attribute("value")
+    can_be_increased = True if max_bin != "200" else False
+    # dont increase when max bin is 200
+    # increase min price according to the loop counter
+    if can_be_increased and increase_price:
+        self.element_actions.execute_element_action(elements.INCREASE_PRICE_BTN, ElementCallback.CLICK)
+    if not increase_price and can_be_decreased:
+        self.element_actions.execute_element_action(elements.DECREASE_PRICE_BTN, ElementCallback.CLICK)
+
+
 def run_loop(self):
     loop_counter = 1
+    increase_min_price = True
     while loop_counter < 10:
         self.element_actions.execute_element_action(elements.SEARCH_PLAYER_BTN, ElementCallback.CLICK)
         # time.sleep(1)
@@ -105,11 +131,15 @@ def run_loop(self):
             self.playerActions.list_player("500")
         else:
             self.element_actions.execute_element_action(elements.NAVIGATE_BACK, ElementCallback.CLICK)
-        #if price is 200 not needed
-        if loop_counter % 2 == 1:
-            self.element_actions.execute_element_action(elements.INCREASE_PRICE_BTN, ElementCallback.CLICK)
-        else:
-            self.element_actions.execute_element_action(elements.DECREASE_PRICE_BTN, ElementCallback.CLICK)
+
+        decrease_increase_min_price(self, increase_min_price)
+        increase_min_price = not increase_min_price
+        # if price is 200 not needed
+
+        # if loop_counter % 2 == 1:
+        #     self.element_actions.execute_element_action(elements.INCREASE_PRICE_BTN, ElementCallback.CLICK)
+        # else:
+        #     self.element_actions.execute_element_action(elements.DECREASE_PRICE_BTN, ElementCallback.CLICK)
         loop_counter += 1
 
 
@@ -147,11 +177,10 @@ class FabDriver:
             self.playerActions = PlayerActions(self.driver)
             self.element_actions.wait_for_page_to_load()
             remove_unexpected_popups(self)
-            self.playerActions.init_search_player_info("tallo", "200")
+            self.playerActions.init_search_player_info("tallo", "600")
             run_loop(self)
             return server_status_messages.FAB_LOOP_FINISHED, 200
 
         except (WebDriverException, TimeoutException) as e:
             print(f"Oops :( Something went wrong.. {e.msg}")
             return server_status_messages.FAB_LOOP_FAILED, 404
-
--- players_actions.py
+++ players_actions.py
@@ -1,15 +1,7 @@
-import time
-from enum import Enum
-
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
 import elements
-import elements_manager
-from elements_manager import ElementPathBy
-
 from driver import Driver
-
 from elements_manager import ElementCallback, ElementActions, ElementPathBy
 
 
@@ -17,62 +9,44 @@ class PlayerActions(Driver):
     def __init__(self, driver):
         self.driver = driver
         super().__init__(driver)
+        self.element_actions = ElementActions(self.driver)
 
     def init_search_player_info(self, player_name, player_price):
         # click on TRANSFERS
-        element_actions = ElementActions(self.driver)
+
         #trying to get rid of this down here with wait_for_element_disapears function - no success
-        element_actions.execute_element_action(elements.ICON_TRANSFER_BTN, ElementCallback.CLICK)
+        self.element_actions.execute_element_action(elements.ICON_TRANSFER_BTN, ElementCallback.CLICK)
         # click on search on transfer market
-        element_actions.execute_element_action(elements.TRANSFER_MARKET_CONTAINER_BTN, ElementCallback.CLICK)
+        self.element_actions.execute_element_action(elements.TRANSFER_MARKET_CONTAINER_BTN, ElementCallback.CLICK)
         # write the searched player name
-        element_actions.execute_element_action(elements.SEARCHED_PLAYER_FIELD, ElementCallback.SEND_KEYS, player_name)
+        self.element_actions.execute_element_action(elements.SEARCHED_PLAYER_FIELD, ElementCallback.SEND_KEYS, player_name)
         # choose the player in the list(the first one)
-        element_actions.execute_element_action(elements.FIRST_RESULT_INPUT_SEARCH, ElementCallback.CLICK)
+        self.element_actions.execute_element_action(elements.FIRST_RESULT_INPUT_SEARCH, ElementCallback.CLICK)
         # set max BIN price - clear the input first
-        element_actions.execute_element_action(elements.MAX_BIN_PRICE_INPUT, ElementCallback.SEND_KEYS, player_price)
+        self.element_actions.execute_element_action(elements.MAX_BIN_PRICE_INPUT, ElementCallback.SEND_KEYS, player_price)
         # set min price - clear the input first
         # element_actions.execute_element_action(elements.MIN_BIN_PRICE_INPUT, ElementCallback.SEND_KEYS, player_price)
 
-
-    def search_player(self, bin_increase=True):
-        # search`
-        element_actions = ElementActions(self.driver)
-        element_actions.execute_element_action(elements.SEARCH_PLAYER_BTN, ElementCallback.CLICK)
-
-        if bin_increase:
-            element_actions.execute_element_action(elements.INCREASE_PRICE_BTN, ElementCallback.CLICK)
-        else:
-            element_actions.execute_element_action(elements.DECREASE_PRICE_BTN, ElementCallback.CLICK)
-
     def buy_player(self):
-        element_actions = ElementActions(self.driver)
-        no_results_banner = element_actions.get_clickable_element(elements.NO_RESULTS_FOUND)
+        no_results_banner = self.element_actions.get_clickable_element(elements.NO_RESULTS_FOUND)
         if no_results_banner:
-            element_actions.execute_element_action(elements.NAVIGATE_BACK, ElementCallback.CLICK)
+            self.element_actions.execute_element_action(elements.NAVIGATE_BACK, ElementCallback.CLICK)
             return False
         else:
-            element_actions.execute_element_action(elements.BUY_BTN, ElementCallback.CLICK)
-            element_actions.execute_element_action(elements.CONFIRM_BUY_BTN, ElementCallback.CLICK)
+            self.element_actions.execute_element_action(elements.BUY_BTN, ElementCallback.CLICK)
+            self.element_actions.execute_element_action(elements.CONFIRM_BUY_BTN, ElementCallback.CLICK)
             return True
 
-        # if no players to the wanted price were found - navigate back
-
     def list_player(self, price):
-        element_actions = ElementActions(self.driver)
         # Button to start the listing quickly after buying (not through user transfer list)
-        element_actions.execute_element_action(elements.LIST_ON_TRANSFER_BTN, ElementCallback.CLICK)
+        self.element_actions.execute_element_action(elements.LIST_ON_TRANSFER_BTN, ElementCallback.CLICK)
 
-        element_actions.execute_element_action(elements.MAX_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, Keys.CONTROL, "a")
-        element_actions.execute_element_action(elements.MAX_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, price)
+        self.element_actions.execute_element_action(elements.MAX_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, Keys.CONTROL, "a")
+        self.element_actions.execute_element_action(elements.MAX_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, price)
 
-
-        element_actions.execute_element_action(elements.MIN_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, Keys.CONTROL, "a")
-        element_actions.execute_element_action(elements.MIN_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, price)
-
-
+        self.element_actions.execute_element_action(elements.MIN_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, Keys.CONTROL, "a")
+        self.element_actions.execute_element_action(elements.MIN_BIN_PRICE_INPUT_AFTER_LIST, ElementCallback.SEND_KEYS, price)
         # List player on transfer market
-        element_actions.execute_element_action(elements.LIST_ITEM_ON_TRANSFER_LIST, ElementCallback.CLICK)
-
+        self.element_actions.execute_element_action(elements.LIST_ITEM_ON_TRANSFER_LIST, ElementCallback.CLICK)
         # Navigate back after player was listed
-        element_actions.execute_element_action(elements.NAVIGATE_BACK, ElementCallback.CLICK)
+        self.element_actions.execute_element_action(elements.NAVIGATE_BACK, ElementCallback.CLICK)
